5e9102da35c1ce3c813c656465954cf17fda23fd
Quvid <48755156+KimKyuHoi@users.noreply.github.com>
2023-09-02 21:26:46 +0900

    [Feat] : 세팅 페이지 구현 및 로그아웃 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import LandingPage from "./components/landingPage/LandingPage";
 import MyPage from "./components/myPage/MyPage";
+import Setting from "./components/setting/Setting";
 import { AnimatePresence } from "framer-motion";
 import { Routes, Route } from "react-router-dom";
 
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/mypage" element={<MyPage />} />
+          <Route path="/setting" element={<Setting />} />
         </Routes>
       </AnimatePresence>
     </>
