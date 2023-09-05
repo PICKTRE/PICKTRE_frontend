0968806f0d678fd02fb80bee6e05e3d2ed4e7e28
junyeokk <cjh4302@gmail.com>
2023-09-05 23:52:03 +0900

    [Refactor]: MyList 페이지 코드 주석 추가

--- src/components/myPage/MyPage.jsx
+++ src/components/myPage/MyPage.jsx
@@ -13,27 +13,32 @@ const contentVariants = {
   visible: {
     opacity: 1,
     transition: { delay: 0.3, duration: 0.5 },
-  }
+  },
 };
 
 const MyPage = () => {
   return (
     <div>
-      <HeaderLayout title="My Page"/>
+      {/* 페이지 상단에 HeaderLayout 컴포넌트를 렌더링하며 'My Page'라는 타이틀을 전달 */}
+      <HeaderLayout title="My Page" />
       <motion.main
         className={classes.main}
         variants={contentVariants}
         initial="hidden"
         animate="visible"
       >
+        {/* 프로필 섹션을 표시하는 MyProfile 컴포넌트 */}
         <section className={classes.profileSection}>
           <MyProfile />
         </section>
+        {/* 메인 하단의 게시물 섹션을 표시하는 MySubPost 컴포넌트 */}
         <section className={classes.mainSubPost}>
           <MySubPost />
         </section>
+        {/* 마이페이지 메뉴 목록을 표시하는 MyList 컴포넌트 */}
         <MyList />
       </motion.main>
+      {/* 페이지 하단에 Footer 컴포넌트를 렌더링 */}
       <Footer />
     </div>
   );
--- src/components/myPage/MyProfile.jsx
+++ src/components/myPage/MyProfile.jsx
@@ -3,84 +3,83 @@ import { useEffect, useState } from "react";
 import showProfile from "../../service/showProfile";
 
 const MyProfile = () => {
-  const [name, setName] = useState("");
-  const [point, setPoint] = useState(0);
-  const [todayPoint, setTodayPoint] = useState(0);
-  const [isLoading, setIsLoading] = useState(true);
-  const [mail, setMail] = useState("");
-  const [image, setImage] = useState("");
-  const [tierImg, setTierImg] = useState("");
+  const [name, setName] = useState(""); // 사용자 이름을 저장하기 위한 상태
+  const [point, setPoint] = useState(0); // 보유 리워드 포인트를 저장하기 위한 상태
+  const [todayPoint, setTodayPoint] = useState(0); // 오늘 얻은 리워드 포인트를 저장하기 위한 상태
+  const [isLoading, setIsLoading] = useState(true); // 데이터 로딩 상태를 관리하기 위한 상태
+  const [mail, setMail] = useState(""); // 사용자 이메일 주소를 저장하기 위한 상태
+  const [image, setImage] = useState(""); // 프로필 이미지 URL을 저장하기 위한 상태
+  const [tierImg, setTierImg] = useState(""); // 사용자 티어 이미지 URL을 저장하기 위한 상태
 
   useEffect(() => {
+    // 사용자 정보를 불러오는 함수 실행
     showProfile()
       .then((data) => {
-        setName(data.data.username);
-        setTodayPoint(data.data.todayReward);
-        setPoint(data.data.rewardPoints);
-        setImage(data.data.picture);
-        setTierImg(data.data.tierPath);
-        setMail(data.data.mail);
-        setTierImg(data.data.tierPath);
-        setIsLoading(false);
-        // console.log("Data loaded:", data.data.rewardPoints, data.data.username);
+        // API로부터 데이터를 성공적으로 가져왔을 때 실행
+        setName(data.data.username); // 이름 업데이트
+        setTodayPoint(data.data.todayReward); // 오늘 얻은 리워드 업데이트
+        setPoint(data.data.rewardPoints); // 보유 리워드 포인트 업데이트
+        setImage(data.data.picture); // 프로필 이미지 URL 업데이트
+        setTierImg(data.data.tierPath); // 사용자 티어 이미지 URL 업데이트
+        setMail(data.data.mail); // 이메일 주소 업데이트
+        setIsLoading(false); // 로딩 상태 해제
       })
       .catch((err) => {
-        console.log(err);
-        setIsLoading(false);
+        console.log(err); // 데이터 가져오기 실패 시 콘솔에 에러 메시지 표시
+        setIsLoading(false); // 로딩 상태 해제
       });
-  }, []);
+  }, []); // 빈 배열을 전달하여 컴포넌트가 마운트될 때 한 번만 실행
 
   return (
     <div className={classes.profileSection}>
-      {/* {console.log("Data loaded:", point, name, image, todayPoint)} */}
       {isLoading ? (
-        <div className={classes.profileImg} />
+        <div className={classes.profileImg} /> // 데이터 로딩 중에는 프로필 이미지를 표시하지 않음
       ) : (
-        <img src={image} alt="profile" className={classes.profileImg} />
+        <img src={image} alt="profile" className={classes.profileImg} /> // 이미지 로딩이 완료되면 프로필 이미지 표시
       )}
       <div className={classes.profileInfoSection}>
         <div className={classes.profile}>
           <ul className={classes.profileInfo}>
             <li>
               {isLoading ? (
-                <div className={classes.name} />
+                <div className={classes.name} /> // 데이터 로딩 중에는 이름을 표시하지 않음
               ) : (
-                <div className={classes.names}>{name}</div>
+                <div className={classes.names}>{name}</div> // 이름 로딩이 완료되면 이름 표시
               )}
             </li>
             <li>
               TODAY 리워드
               {isLoading ? (
-                <div className={classes.todayReward} />
+                <div className={classes.todayReward} /> // 데이터 로딩 중에는 오늘 얻은 리워드 포인트를 표시하지 않음
               ) : (
-                <div className={classes.todayRewards}>{todayPoint}P</div>
+                <div className={classes.todayRewards}>{todayPoint}P</div> // 리워드 포인트 로딩이 완료되면 오늘 얻은 리워드 포인트 표시
               )}
             </li>
             <li>
               보유리워드{" "}
               {isLoading ? (
-                <div className={classes.totalReward} />
+                <div className={classes.totalReward} /> // 데이터 로딩 중에는 보유 리워드 포인트를 표시하지 않음
               ) : (
-                <div className={classes.totalRewardS}>{point}P</div>
+                <div className={classes.totalRewardS}>{point}P</div> // 보유 리워드 포인트 로딩이 완료되면 보유 리워드 포인트 표시
               )}
             </li>
           </ul>
           <div className={classes.profileTier}>
             {isLoading ? (
-              <div className={classes.profileTierImg} />
+              <div className={classes.profileTierImg} /> // 데이터 로딩 중에는 티어 이미지를 표시하지 않음
             ) : (
               <img
                 src={tierImg}
                 alt="티어 이미지 입니다."
-                className={classes.profileTierImg}
+                className={classes.profileTierImg} // 티어 이미지 로딩이 완료되면 티어 이미지 표시
               />
             )}
           </div>
         </div>
         {isLoading ? (
-          <button className={classes.verify}>이메일을 인증해주세요</button>
+          <button className={classes.verify}>이메일을 인증해주세요</button> // 데이터 로딩 중에는 이메일 주소를 표시하지 않음
         ) : (
-          <button className={classes.verify}>{mail}</button>
+          <button className={classes.verify}>{mail}</button> // 이메일 주소 로딩이 완료되면 이메일 주소 표시
         )}
       </div>
     </div>
