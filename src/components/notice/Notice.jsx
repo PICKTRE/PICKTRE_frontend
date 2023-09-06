36bd344436c331abf00e379a5abf1db9cd294d69
junyeokk <cjh4302@gmail.com>
2023-09-07 00:19:47 +0900

    [Refactor]: Notice 페이지 코드 주석 추가

--- src/components/notice/Notice.jsx
+++ src/components/notice/Notice.jsx
@@ -6,6 +6,7 @@ import AccordionList from "../common/AccordionList";
 import dummyData from "./dummy/noticeData.json";
 
 const Notice = () => {
+  // 콘텐츠 애니메이션을 위한 변수와 설정
   const contentVariants = {
     hidden: {
       opacity: 0.3,
@@ -18,6 +19,7 @@ const Notice = () => {
 
   return (
     <>
+      {/* 페이지 제목과 레이아웃 헤더 */}
       <HeaderLayout title="Notice" />
       <motion.main
         className={classes.main}
@@ -25,6 +27,7 @@ const Notice = () => {
         initial="hidden"
         animate="visible"
       >
+        {/* 공지사항 섹션 */}
         <div className={classes.noticeSection}>
           <div className={classes.noticeMainText}>
             공지사항
@@ -33,9 +36,11 @@ const Notice = () => {
             </div>
           </div>
 
+          {/* 아코디언 형태의 공지사항 목록 */}
           <AccordionList dummyData={dummyData} />
         </div>
       </motion.main>
+      {/* 공통 푸터 */}
       <Footer />
     </>
   );
