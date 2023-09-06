6bbc9ef2c9e3394211eb6c615a3d923673d5b649
junyeokk <cjh4302@gmail.com>
2023-09-07 00:15:44 +0900

    [Refactor]: Invite Friends 페이지 코드 주석 추가

--- src/components/inviteFriends/InviteFriends.jsx
+++ src/components/inviteFriends/InviteFriends.jsx
@@ -19,16 +19,15 @@ const contentVariants = {
 };
 
 const InviteFriends = () => {
-
-
-
+  // 상태 변수 및 이미지 로딩 상태 초기화
   const [isLoading, setIsLoading] = useState(true);
   const [image, setImage] = useState("");
 
-
   useEffect(() => {
+    // 프로필 정보를 불러오는 비동기 함수 실행
     showProfile()
       .then((data) => {
+        // 프로필 이미지 및 로딩 상태 업데이트
         setImage(data.data.picture);
         setIsLoading(false);
         // console.log("Data loaded:", data.data.rewardPoints, data.data.username);
@@ -42,7 +41,9 @@ const InviteFriends = () => {
   return (
     <>
       <div>
-        <HeaderLayout title="Invite Friends" />
+        {/* 페이지 헤더 */}
+        <HeaderLayout title="친구 초대" />
+        {/* 페이지 내용 */}
         <motion.main
           className={classes.main}
           variants={contentVariants}
@@ -51,17 +52,28 @@ const InviteFriends = () => {
         >
           <div className={classes.whiteSection} />
           <img src={inviteP} alt="소개 이미지" className={classes.imgs} />
+          {/* 프로필 섹션 */}
           <section className={classes.profileSection}>
-            {isLoading ? 
-            (<div className={classes.Myprofile} />) : 
-            (<img src={image} className={classes.Myprofile} />)
-            }
-            <img src={friends_profile} alt="친구 이미지" className={classes.Otherprofile} />
+            {isLoading ? (
+              // 로딩 중인 경우 빈 프로필 이미지
+              <div className={classes.Myprofile} />
+            ) : (
+              // 이미지 로딩이 완료된 경우 프로필 이미지 표시
+              <img src={image} className={classes.Myprofile} />
+            )}
+            {/* 친구 프로필 이미지 */}
+            <img
+              src={friends_profile}
+              alt="친구 이미지"
+              className={classes.Otherprofile}
+            />
           </section>
+          {/* 초대 버튼 섹션 */}
           <section className={classes.lastSection}>
             <InviteFriendsButton />
           </section>
         </motion.main>
+        {/* 페이지 푸터 */}
         <Footer />
       </div>
     </>
--- src/components/inviteFriends/InviteFriendsButton.jsx
+++ src/components/inviteFriends/InviteFriendsButton.jsx
@@ -1,22 +1,26 @@
 import classes from "./InviteFriendsButton.module.css";
 import { motion } from "framer-motion";
 
+// 클립보드에 텍스트 복사하는 함수
 const handleCopyClipBoard = async () => {
   try {
     await navigator.clipboard.writeText("https://picktre.netlify.app");
     console.log("복사에 성공했습니다.");
-    alert("복사에 성공했습니다.")
+    alert("복사에 성공했습니다.");
   } catch (e) {
     console.log("복사에 실패했습니다.");
-    alert("복사에 실패했습니다.")
+    alert("복사에 실패했습니다.");
   }
 };
 
 const InviteFriendsButton = () => {
   return (
     <>
+      {/* 클릭 시 클립보드에 링크 복사 */}
       <div className={classes.container} onClick={handleCopyClipBoard}>
+        {/* 버튼 스케일 애니메이션 */}
         <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.8 }}>
+          {/* 버튼 텍스트 */}
           <span className={classes.mainText}>
             링크 복사해서 친구들에게 공유하기
           </span>
